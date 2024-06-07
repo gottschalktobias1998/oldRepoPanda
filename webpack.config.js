74385bf1ce263fa3e1f345eacdd39c9ebf6fe58c
Tobias Gottschalk <gottschalk.tobias98@outlook.de>
2024-06-07 07:26:08 +0200

    fix webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,11 @@ module.exports = {
     })
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
     compress: true,
-    port: 9000
+    port: 9000,
+    open: true
   }
 };
